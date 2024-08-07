6565123d5734ddeff3ad06458064d4f41ac1345b
Gearóid O'Ceallacháin <gocealla@gmail.com>
2024-08-07 19:01:29 +0300

    Revert "created a div to have search-results and playlist side by side"
    This reverts commit eb37a98de0c0549e1120fdbe14be18fc27dbf2f0.

--- src/App.jsx
+++ src/App.jsx
@@ -10,6 +10,7 @@ const [results, setResults] = useState([]);
       <div className="App">
         <div className='search-bar-container'>
           <SearchBar setResults={setResults} />
+          <div className='search-results-playlist-list'>
               <SearchResultsList results={results} />
           </div>
          
