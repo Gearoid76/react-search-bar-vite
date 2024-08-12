3ad478164f862ec8ee517a1a9bedfd7ecbc9461e
Gearóid O'Ceallacháin <gocealla@gmail.com>
2024-08-12 11:10:03 +0300

    Had the code written twice

--- src/components/SearchResult.jsx
+++ src/components/SearchResult.jsx
@@ -1,19 +1,3 @@
-// src/components/SearchResult.jsx
-import React from 'react';
-
-export const SearchResult = ({ result }) => {
-  return (
-    <div className='search-result'>
-      <img src={result.imageUrl} alt={result.name} style={{ width: '50px', height: '50px' }} />
-      <div>
-        <h3>{result.name}</h3>
-        <p>{result.artist} - {result.album}</p>
-        <a href={result.externalUrl} target="_blank" rel="noopener noreferrer">Listen on Spotify</a>
-      </div>
-    </div>
-  );
-};
-
 // src/components/SearchResultsList.jsx
 import React from 'react';
 import { SearchResult } from './SearchResult';
