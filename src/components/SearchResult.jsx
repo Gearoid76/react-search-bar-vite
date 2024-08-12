d7a0f8fb6ae99ee2ac549e5ae2b3c5e6236c301c
Gearóid O'Ceallacháin <gocealla@gmail.com>
2024-08-12 11:04:10 +0300

    updating SearchResult.jsx

--- src/components/SearchResult.jsx
+++ src/components/SearchResult.jsx
@@ -1,10 +1,29 @@
-//src/components/SearchResult.jsx
-import React from 'react'
-import { FaPlus } from "react-icons/fa";
-import App from '../App';
+// src/components/SearchResult.jsx
+import React from 'react';
 
-export const SearchResult = ({result}) => {
+export const SearchResult = ({ result }) => {
   return (
-    <div className='search-result'>{result.name} <FaPlus id='add-icon' onClick={(e) => alert(`You clicked on ${result.name}`)}/> </div>
-  )
-}
+    <div className='search-result'>
+      <img src={result.imageUrl} alt={result.name} style={{ width: '50px', height: '50px' }} />
+      <div>
+        <h3>{result.name}</h3>
+        <p>{result.artist} - {result.album}</p>
+        <a href={result.externalUrl} target="_blank" rel="noopener noreferrer">Listen on Spotify</a>
+      </div>
+    </div>
+  );
+};
+
+// src/components/SearchResultsList.jsx
+import React from 'react';
+import { SearchResult } from './SearchResult';
+
+export const SearchResultsList = ({ results }) => {
+  return (
+    <div className='results-list'>
+      {results.map((result, id) => (
+        <SearchResult result={result} key={id} />
+      ))}
+    </div>
+  );
+};
